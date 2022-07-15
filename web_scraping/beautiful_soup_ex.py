3dd4e93b6abe82bb143f9d6276a74fb6a9382d28
CairoDeAndrade <cairo.andrade777@gmail.com>
2022-07-15 17:21:15 -0300

    Update beautiful_soup_ex.py

--- web_scraping/beautiful_soup_ex.py
+++ web_scraping/beautiful_soup_ex.py
@@ -6,26 +6,50 @@ products = []
 prices = []
 general = []
 
-for i in range(100):
-    target = f'https://www.havan.com.br/eletro/?departamentosHV=&p={i}'
-    response = requests.get(target)
-    html = BeautifulSoup(response.text, 'html.parser')
+nex_page = requests.get('https://www.havan.com.br/eletro/?departamentosHV=&p=')
+cont = 1
+if nex_page is not None:
+    while True:
+        target = f'https://www.havan.com.br/eletro/?departamentosHV=&p={cont}'
+        response = requests.get(target)
+        html = BeautifulSoup(response.text, 'html.parser')
 
-    for product in html.select('.product-item-link'):
-        products.append(product.text.replace(' ', '').replace('\n', ''))
+        for product in html.select('.product-item-link'):
+            products.append(product.text.replace(' ', '').replace('\n', ''))
 
-    for value in html.select('.weee:nth-child(1) .price'):
-        prices.append(float(value.text.replace('.', '').replace('R$\xa0', '').replace(',', '.')))
+        for value in html.select('.weee:nth-child(1) .price'):
+            prices.append(float(value.text.replace('.', '').replace('R$\xa0', '').replace(',', '.')))
 
 
 for i in range(len(products)):
     new_registry = {'Product': products[i], 'Price': prices[i]}
     general.append(new_registry)
 
-
 final_list = sorted(general, key=itemgetter('Price'), reverse=True)
 for i in final_list:
     print(i)
+
+
+# for i in range(100):
+#     target = f'https://www.havan.com.br/eletro/?departamentosHV=&p={i}'
+#     response = requests.get(target)
+#     html = BeautifulSoup(response.text, 'html.parser')
+#
+#     for product in html.select('.product-item-link'):
+#         products.append(product.text.replace(' ', '').replace('\n', ''))
+#
+#     for value in html.select('.weee:nth-child(1) .price'):
+#         prices.append(float(value.text.replace('.', '').replace('R$\xa0', '').replace(',', '.')))
+#
+#
+# for i in range(len(products)):
+#     new_registry = {'Product': products[i], 'Price': prices[i]}
+#     general.append(new_registry)
+#
+#
+# final_list = sorted(general, key=itemgetter('Price'), reverse=True)
+# for i in final_list:
+#     print(i)
 
 
 

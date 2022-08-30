56c67c7ab854000677fd65603713d4ed837061d8
CairoDeAndrade <cairo.andrade777@gmail.com>
2022-08-30 17:25:36 -0300

    django

---
+++ django-aula3/contacts/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20, blank=True)
+    cel = models.CharField(max_length=15)
+    mail = models.EmailField()
+    register_date = models.DateField(default=timezone.now)
+    description = models.TextField(blank=True)
+    categories = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.name
+
+
